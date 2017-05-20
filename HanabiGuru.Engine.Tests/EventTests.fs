d68cdf94748f687a0327e75b2eff590183c161d2
Nicholas Lawton <nick.a.lawton@gmail.com>
2017-05-20 17:27:31 +1000

    FsCheck installation

--- HanabiGuru.Engine.Tests/EventTests.fs
+++ HanabiGuru.Engine.Tests/EventTests.fs
@@ -4,7 +4,7 @@ open FsCheck
 open FsCheck.Xunit
 
 [<Property>]
-let ``When a card is added to the draw deck, the draw deck contains the added card`` =
+let ``When a card is added to the draw deck, the draw deck contains the added card`` () =
     let addedCard = Card(Suit.Red, 2)
     let cardAdded = CardAddedToDrawDeck addedCard
     let gameState = GameState.Initial
