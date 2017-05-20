cd69942837ecbac5e3008d2a22a4b19116851eec
Nicholas Lawton <nick.a.lawton@gmail.com>
2017-05-20 17:24:49 +1000

    Initial test file

---
+++ HanabiGuru.Engine.Tests/EventTests.fs
@@ -0,0 +1,14 @@
+﻿module HanabiGuru.Engine.Tests.EventTests
+
+open FsCheck
+open FsCheck.Xunit
+
+[<Property>]
+let ``When a card is added to the draw deck, the draw deck contains the added card`` =
+    let addedCard = Card(Suit.Red, 2)
+    let cardAdded = CardAddedToDrawDeck addedCard
+    let gameState = GameState.Initial
+
+    var newState = Engine.processEvent gameState cardAdded
+
+    newState.drawDeck = [addedCard]
