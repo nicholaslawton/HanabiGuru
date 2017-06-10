a2402f4234fa1c960a97a9c153cddd9480b5db9d
Nicholas Lawton <nick.a.lawton@gmail.com>
2017-06-10 17:16:12 +1000

    Console client tests

---
+++ HanabiGuru.Client.Console.Tests/CommandInterfaceTests.fs
@@ -0,0 +1,25 @@
+﻿module HanabiGuru.Client.Console.Tests.CommandInterfaceTests
+
+open FsCheck.Xunit
+open Swensen.Unquote
+open HanabiGuru.Client.Console
+
+[<Property>]
+let ``Command processing processes commands received and then terminates`` (commands : string list) =
+    let mutable commandsQueue = commands
+    let mutable commandsProcessed = []
+
+    let processCommand command = commandsProcessed <- command :: commandsProcessed
+    let pipeline stream = stream |> Observable.subscribe processCommand
+
+    let getInput () =
+        match commandsQueue with
+        | command :: remainingCommands ->
+            commandsQueue <- remainingCommands
+            Some command
+        | [] -> None
+
+    CommandInterface.processCommands getInput pipeline
+
+    commandsQueue =! []
+    List.rev commandsProcessed =! commands
