61427a56c477cc823d10728c0c87938bb5d0710b
Nicholas Lawton <nick.a.lawton@gmail.com>
2017-06-10 17:00:36 +1000

    Created console shell

---
+++ HanabiGuru.Client.Console/CommandInterface.fs
@@ -0,0 +1,20 @@
+﻿module HanabiGuru.Client.Console.CommandInterface
+
+type private EventStream<'a> () = 
+
+    let events = new Event<'a>() 
+ 
+    [<CLIEvent>] 
+    member this.EventStream = events.Publish 
+
+    member this.RaiseEvent event = events.Trigger event
+
+let processCommands getInput pipeline =
+    let stream = EventStream ()
+    use subscription = stream.EventStream |> pipeline
+    let rec processNextCommand () =
+        getInput ()
+        |> Option.iter (fun command ->
+            stream.RaiseEvent command
+            processNextCommand () |> ignore)
+    processNextCommand ()
---
+++ HanabiGuru.Client.Console/Program.fs
@@ -0,0 +1,12 @@
+﻿open System
+open HanabiGuru.Client.Console
+
+[<EntryPoint>]
+let main _ = 
+    let pipeline stream = stream |> Observable.subscribe(printfn "%s")
+    let getInput () =
+        match Console.ReadLine() with
+        | "exit" -> None
+        | line -> Some line
+    CommandInterface.processCommands getInput pipeline
+    0 // return an integer exit code
