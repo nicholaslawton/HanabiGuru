fd3de31d062bc88e001a5e35160af341ee2f8a47
Nicholas Lawton <nick.a.lawton@gmail.com>
2017-07-24 22:44:56 +1000

    Separation of initial deal integration test

--- HanabiGuru.Client.Console.Tests/StartGameIntegrationTests.fs
+++ HanabiGuru.Client.Console.Tests/StartGameIntegrationTests.fs
@@ -7,18 +7,24 @@ open HanabiGuru.Engine
 open HanabiGuru.Client.Console
 open HanabiGuru.Engine.Tests
 
+let private startGame names =
+    StartGame :: List.map AddPlayer names
+    |> List.rev
+    |> CommandExecutionTestFramework.execute
+
+let private hasEmptyHand player = List.isEmpty player.hand
+
 [<Property(Arbitrary = [| typeof<DistinctPlayers> |])>]
-let ``After starting the game, all players have cards in their hands`` (CompletePlayerNames names) =
-    let commands = (List.map AddPlayer names) @ [StartGame]
-    let game = CommandExecutionTestFramework.execute commands
+let ``After starting the game, all players have cards in their hands in the master view`` (CompletePlayerNames names) =
+    startGame names |> fun game -> game.masterView.players |> List.filter hasEmptyHand =! []
 
-    let hasEmptyHand player = List.isEmpty player.hand
+[<Property(Arbitrary = [| typeof<DistinctPlayers> |])>]
+let ``After starting the game, all players have cards in their own hands`` (CompletePlayerNames names) =
+    startGame names |> fun game -> game.playerViews |> List.filter (fun view -> hasEmptyHand view.self) =! []
 
-    [
-        game.masterView.players |> List.filter hasEmptyHand =! []
-            |@ "No empty hands in master view"
-        game.playerViews |> List.filter (fun view -> hasEmptyHand view.self) =! []
-            |@ "No empty hands for self"
-        game.playerViews |> List.filter (fun view -> view.otherPlayers |> List.exists hasEmptyHand) =! []
-            |@ "No empty hands for other players"
-    ]
+[<Property(Arbitrary = [| typeof<DistinctPlayers> |])>]
+let ``After starting the game, all players see cards in the hands of all other players`` (CompletePlayerNames names) =
+    startGame names
+    |> fun game -> game.playerViews
+    |> List.filter (fun view -> view.otherPlayers |> List.exists hasEmptyHand) =! []
+
