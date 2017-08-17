c6b9825db7921811f0f8d04daeba5fc67cc5af80
Nicholas Lawton <nick.a.lawton@gmail.com>
2017-08-17 23:16:24 +1000

    Presentation of players waiting to start game

--- HanabiGuru.Client.Console/Presentation.fs
+++ HanabiGuru.Client.Console/Presentation.fs
@@ -5,40 +5,43 @@ open HanabiGuru.Engine
 
 let private task f x = fun () -> f x
 
-let private presentPlayers = task (printfn "%A")
+let lineBreakTask = task printfn ""
 
-let private presentPlayerView =
-    let presentNameTask (name : string) =
+let private playersTasks players =
+    let playerTask (Name name) = task (printf "%s") name
+    task printf "Players: "
+    :: (Set.toList players |> List.map playerTask |> List.weave (task printf ", "))
+
+let private playerViewTasks =
+    let nameTask (name : string) =
         let nameWidth = 10
         name |> Seq.truncate nameWidth |> String.Concat |> task (printf "%*s" nameWidth)
-    let presentCardTask (Card (suit, Rank rank)) =
+    let cardTask (Card (suit, Rank rank)) =
         [
             suit |> sprintf "%A" |> Seq.take 1 |> String.Concat |> task (printf "%s")
             rank |> task (printf "%i")
         ]
         |> List.reduce (>>)
-    let presentHandTasks { player = Name name; cards = hand } =
-        presentNameTask name
+    let handTasks { player = Name name; cards = hand } =
+        nameTask name
         :: task printf ": "
-        :: List.weave (task printf " ") (List.map (presentCardTask) hand)
-    let lineBreakTask = task printfn ""
+        :: List.weave (task printf " ") (List.map cardTask hand)
 
     PlayerView.otherHands
     >> function
         | [] -> PlayerHand.create (PlayerIdentity.create "") [] |> List.singleton
         | hands -> hands
-    >> List.map presentHandTasks
+    >> List.map handTasks
     >> List.map (List.reduce (>>))
     >> List.weave lineBreakTask
-    >> List.reduce (>>)
-    >> ((<<) lineBreakTask)
 
 let game state =
-    let present =
+    let tasks =
         match GameState.activePlayer state with
-        | Some player -> GameState.playerView player state |> presentPlayerView
-        | None -> GameState.players state |> presentPlayers
-    present ()
+        | Some player -> GameState.playerView player state |> playerViewTasks
+        | None -> GameState.players state |> playersTasks
+    let composition = tasks |> List.reduce (>>) >> lineBreakTask
+    composition ()
 
 let commandFailure (failure : CannotPerformAction) = printfn "%A" failure
 
