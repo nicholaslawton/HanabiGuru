0151c56db43067466131f483c1a5a851fa46a4cc
Nicholas Lawton <nick.a.lawton@gmail.com>
2017-08-18 22:24:43 +1000

    Fixed valid name generation

--- HanabiGuru.Client.Console.Tests/InputGeneration.fs
+++ HanabiGuru.Client.Console.Tests/InputGeneration.fs
@@ -8,20 +8,22 @@ type Name = Name of string
 type Names = Names of Set<string>
 
 type InputGeneration =
-    static member private removeNewLines (NonEmptyString s) =
-        s |> Seq.toList |> List.removeEach (Seq.toList Environment.NewLine) |> string
+    static member private nameGenerator =
+        Arb.generate<char>
+        |> Gen.filter (fun c -> ' ' <= c && c <= '~')
+        |> Gen.nonEmptyListOf
+        |> Gen.map string
+        |> Gen.filter (not << String.IsNullOrWhiteSpace)
 
     static member Name() =
-        Arb.generate<NonEmptyString>
-        |> Gen.map InputGeneration.removeNewLines
-        |> Gen.filter (not << String.IsNullOrWhiteSpace)
+        InputGeneration.nameGenerator
         |> Gen.map Name
         |> Arb.fromGen
 
     static member Names() =
-        Arb.generate<Set<NonEmptyString>>
-        |> Gen.map (Set.map InputGeneration.removeNewLines)
-        |> Gen.map (Set.filter (not << String.IsNullOrWhiteSpace))
+        InputGeneration.nameGenerator
+        |> Gen.nonEmptyListOf
+        |> Gen.map set
         |> Gen.filter (Set.count >> ((<=) GameRules.minimumPlayers)) 
         |> Gen.filter (Set.count >> ((>=) GameRules.maximumPlayers)) 
         |> Gen.map Names
