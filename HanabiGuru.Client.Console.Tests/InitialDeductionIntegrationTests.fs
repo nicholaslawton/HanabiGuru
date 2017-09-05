432571755b9141fb1c61942245c3e52461d56502
Nicholas Lawton <nick.a.lawton@gmail.com>
2017-09-05 23:14:11 +1000

    Refactored deduction integration test

--- HanabiGuru.Client.Console.Tests/InitialDeductionIntegrationTests.fs
+++ HanabiGuru.Client.Console.Tests/InitialDeductionIntegrationTests.fs
@@ -8,7 +8,8 @@ let ``After starting a game, players begin deducing the identities of their card
     let startedGame = CommandProcessingTestFramework.startGame names
     GameState.players startedGame
     |> Set.toList
-    |> List.map (fun player -> GameState.playerView player startedGame)
-    |> List.map (fun view -> (PlayerView.hand view, view))
-    |> List.map (fun (cards, view) -> List.map (PlayerView.CardIdentity.deduce view) cards)
+    |> List.map (fun player ->
+        let view = GameState.playerView player startedGame
+        PlayerView.hand view
+        |> List.map (PlayerView.CardIdentity.deduce view))
     |> List.forall (not << List.isEmpty)
